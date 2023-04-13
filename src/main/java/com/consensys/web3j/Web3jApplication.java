04e4864cc37e7002e5ed695268abc1e7789fabc1
Alain Nicolas <alain.nicolas@consensys.net>
2023-04-13 19:09:46 +0200

    ✨ Listen the new version of the 'Created' event

--- src/main/java/com/consensys/web3j/Web3jApplication.java
+++ src/main/java/com/consensys/web3j/Web3jApplication.java
@@ -9,6 +9,7 @@ import org.web3j.abi.FunctionReturnDecoder;
 import org.web3j.abi.TypeReference;
 import org.web3j.abi.datatypes.Address;
 import org.web3j.abi.datatypes.Event;
+import org.web3j.abi.datatypes.Type;
 import org.web3j.abi.datatypes.Utf8String;
 import org.web3j.protocol.Web3j;
 import org.web3j.protocol.core.DefaultBlockParameter;
@@ -27,7 +28,6 @@ public class Web3jApplication {
     @Value("${infuraApiKey}")
     private String infuraApiKey;
 
-    private final String contractAddress = "0x2Ec762F47829b3C46c58bcC6d25d74E9Ed0e110C";
 
     public static void main(String[] args) {
         SpringApplication.run(Web3jApplication.class, args);
@@ -35,13 +35,15 @@ public class Web3jApplication {
 
     @PostConstruct
     public void afterInitialize() {
+        final String factoryAddress = "0x0CB49aEb4069291D27330e1BF524de8adF472e57";
+        final int fromBlockNumber = 3283275;
 
         Web3j web3j = Web3j.build(new HttpService("https://sepolia.infura.io/v3/" + infuraApiKey));
 
         // Define the event we want to listen
         final Event createdEvent = new Event("Created",
                 Arrays.asList(
-                        new TypeReference<Utf8String>(true) {
+                        new TypeReference<Utf8String>(false) {
                         },
                         new TypeReference<Address>(true) {
                         }));
@@ -50,18 +52,24 @@ public class Web3jApplication {
         final String encodedEvent = EventEncoder.encode(createdEvent);
 
         // Create a filter on latest block for the listened contract
-        EthFilter filter = new EthFilter(DefaultBlockParameter.valueOf(Numeric.toBigInt("321464")), DefaultBlockParameterName.LATEST, contractAddress).addSingleTopic(encodedEvent);
+        EthFilter filter = new EthFilter(DefaultBlockParameter.valueOf(Numeric.toBigInt(Integer.toHexString(fromBlockNumber))), DefaultBlockParameterName.LATEST, factoryAddress).addSingleTopic(encodedEvent);
 
         // Listen to events
         web3j.ethLogFlowable(filter).subscribe(event -> {
             List<String> topics = event.getTopics();
             String eventSignature = topics.get(0);
-            String hashedFilmId = topics.get(1);
-            Address deFilmFunding = (Address) FunctionReturnDecoder.decodeIndexedValue(topics.get(2), new TypeReference<Address>() {
+            Address deFilmFunding = (Address) FunctionReturnDecoder.decodeIndexedValue(topics.get(1), new TypeReference<Address>() {
             });
 
-            out.println("Event signature is " + (eventSignature.equals(encodedEvent) ? "matching" : "not matching" + " what we want"));
-            out.println("Block #" + event.getBlockNumber().toString() + " contains a film creation with contract deployed at " + deFilmFunding + " and hashed film ID is " + hashedFilmId);
+            List<Type> nonIndexedValues = FunctionReturnDecoder.decode(
+                    event.getData(), createdEvent.getNonIndexedParameters());
+
+            String hashedFilmId = "";
+            if (nonIndexedValues.size() == 1) {
+                hashedFilmId = nonIndexedValues.get(0).toString();
+            }
+
+            out.println("Event signature is " + (!eventSignature.equals(encodedEvent) ? "not matching" : "matching => Block #" + event.getBlockNumber().toString() + " contains a film creation with contract deployed at " + deFilmFunding + " and film ID is " + hashedFilmId));
         });
     }
 }
