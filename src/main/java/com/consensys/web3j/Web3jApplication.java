a22b743b38f2da9582e9e4d9e1062f74fba67029
Alain Nicolas <alain.nicolas@consensys.net>
2023-04-13 17:14:09 +0200

    ✨ Listen to 'Created' event on the factory

--- src/main/java/com/consensys/web3j/Web3jApplication.java
+++ src/main/java/com/consensys/web3j/Web3jApplication.java
@@ -1,6 +1,5 @@
 package com.consensys.web3j;
 
-import io.reactivex.disposables.Disposable;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
@@ -10,15 +9,14 @@ import org.web3j.abi.FunctionReturnDecoder;
 import org.web3j.abi.TypeReference;
 import org.web3j.abi.datatypes.Address;
 import org.web3j.abi.datatypes.Event;
-import org.web3j.abi.datatypes.Type;
-import org.web3j.abi.datatypes.generated.Uint256;
+import org.web3j.abi.datatypes.Utf8String;
 import org.web3j.protocol.Web3j;
+import org.web3j.protocol.core.DefaultBlockParameter;
 import org.web3j.protocol.core.DefaultBlockParameterName;
 import org.web3j.protocol.core.methods.request.EthFilter;
 import org.web3j.protocol.http.HttpService;
-import org.web3j.utils.Convert;
+import org.web3j.utils.Numeric;
 
-import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.List;
 
@@ -29,7 +27,7 @@ public class Web3jApplication {
     @Value("${infuraApiKey}")
     private String infuraApiKey;
 
-    private final String contractAddress = "0xdAC17F958D2ee523a2206206994597C13D831ec7";
+    private final String contractAddress = "0x2Ec762F47829b3C46c58bcC6d25d74E9Ed0e110C";
 
     public static void main(String[] args) {
         SpringApplication.run(Web3jApplication.class, args);
@@ -38,46 +36,32 @@ public class Web3jApplication {
     @PostConstruct
     public void afterInitialize() {
 
-        Web3j web3j = Web3j.build(new HttpService("https://mainnet.infura.io/v3/" + infuraApiKey));
-
-        // Listen to blocks
-        Disposable blockSubscription = web3j.blockFlowable(false).subscribe(block -> {
-            out.println("Block #" + block.getBlock().getNumber().toString() + " with hash = " + block.getBlock().getHash() + " contains " + block.getBlock().getTransactions().size() + " transactions");
-        });
+        Web3j web3j = Web3j.build(new HttpService("https://sepolia.infura.io/v3/" + infuraApiKey));
 
         // Define the event we want to listen
-        final Event transferEvent = new Event("Transfer",
+        final Event createdEvent = new Event("Created",
                 Arrays.asList(
-                        new TypeReference<Address>(true) {
+                        new TypeReference<Utf8String>(true) {
                         },
                         new TypeReference<Address>(true) {
-                        },
-                        new TypeReference<Uint256>(false) {
                         }));
 
         // Encode the event
-        final String encodedEvent = EventEncoder.encode(transferEvent);
+        final String encodedEvent = EventEncoder.encode(createdEvent);
 
         // Create a filter on latest block for the listened contract
-        EthFilter filter = new EthFilter(DefaultBlockParameterName.LATEST, DefaultBlockParameterName.LATEST, contractAddress).addSingleTopic(encodedEvent);
+        EthFilter filter = new EthFilter(DefaultBlockParameter.valueOf(Numeric.toBigInt("321464")), DefaultBlockParameterName.LATEST, contractAddress).addSingleTopic(encodedEvent);
 
         // Listen to events
-        Disposable transferSubscription = web3j.ethLogFlowable(filter).subscribe(event -> {
+        web3j.ethLogFlowable(filter).subscribe(event -> {
             List<String> topics = event.getTopics();
-            Address from = (Address) FunctionReturnDecoder.decodeIndexedValue(topics.get(1), new TypeReference<Address>() {
+            String eventSignature = topics.get(0);
+            String hashedFilmId = topics.get(1);
+            Address deFilmFunding = (Address) FunctionReturnDecoder.decodeIndexedValue(topics.get(2), new TypeReference<Address>() {
             });
-            Address to = (Address) FunctionReturnDecoder.decodeIndexedValue(topics.get(2), new TypeReference<Address>() {
-            });
 
-            List<Type> nonIndexedValues = FunctionReturnDecoder.decode(
-                    event.getData(), transferEvent.getNonIndexedParameters());
-
-            BigDecimal value = BigDecimal.ZERO;
-            if (nonIndexedValues.size() == 1) {
-                value = new BigDecimal(((Uint256) nonIndexedValues.get(0)).getValue());
-            }
-
-            out.println("Block #" + event.getBlockNumber().toString() + " - Transfer " + Convert.fromWei(value, Convert.Unit.MWEI) + " USDT from " + from + " to " + to);
+            out.println("Event signature is " + (eventSignature.equals(encodedEvent) ? "matching" : "not matching" + " what we want"));
+            out.println("Block #" + event.getBlockNumber().toString() + " contains a film creation with contract deployed at " + deFilmFunding + " and hashed film ID is " + hashedFilmId);
         });
     }
 }
